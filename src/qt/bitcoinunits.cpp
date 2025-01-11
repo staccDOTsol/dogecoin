248f60f35dec2b1fca72e53226b05b7bc0954bb0
jarett dunn <jarettdunn@192.168.1.130>
2025-01-11 00:06:15 +0000

    ic

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -45,11 +45,11 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case MBTC: return QString("MDOGE");
-    case kBTC: return QString("kDOGE");
-    case BTC: return QString("DOGE");
-    case mBTC: return QString("mDOGE");
-    case uBTC: return QString::fromUtf8("μDOGE");
+    case MBTC: return QString("MEGOD");
+    case kBTC: return QString("kEGOD");
+    case BTC: return QString("EGOD");
+    case mBTC: return QString("mEGOD");
+    case uBTC: return QString::fromUtf8("μEGOD");
     default: return QString("???");
     }
 }
@@ -58,11 +58,11 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case MBTC: return QString("Mega-Dogecoins (1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
-    case kBTC: return QString("Kilo-Dogecoins (1" THIN_SP_UTF8 "000)");
-    case BTC: return QString("Dogecoins");
-    case mBTC: return QString("Milli-Dogecoins (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Dogecoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case MBTC: return QString("Mega-Egodcoins (1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case kBTC: return QString("Kilo-Egodcoins (1" THIN_SP_UTF8 "000)");
+    case BTC: return QString("Egodcoins");
+    case mBTC: return QString("Milli-Egodcoins (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Micro-Egodcoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
